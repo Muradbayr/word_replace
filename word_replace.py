5101ef3024cc903a54e14e817bbe842e7b889dff
mura <muradfreelancer1@gmail.com>
2024-12-21 16:49:27 +0300

    First commit

---
+++ word_replace.py
@@ -0,0 +1,10 @@
+
+def replace_word():
+    
+    str = "Hi guys, i am mura bot, and i'm gonna get you too"
+    word_to_replace =input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+    
+replace_word()
